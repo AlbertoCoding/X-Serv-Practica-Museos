f1499c202dd41d2fcf221a54820df27f766d4741
AlbertoCoding <albertor.dev@gmail.com>
2018-04-28 03:41:48 +0200

    Redirect each museum to its url

--- museosMadrid/urls.py
+++ museosMadrid/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     url(r'^about$', views.about),  
   #  url(r'^usuario$', views. ),
   #  url(r'^usuario/', views.)
-  #  url(r'^museos$', views. ),
-  #  url(r'^museos/', views. ),
+    url(r'^museos$', views.todosLosMuseos),
+    url(r'^museos/', views.infoMuseo),
     url(r'^$', views.home),
     url(r'^', views.error)
 ]
--- museosMadrid/views.py
+++ museosMadrid/views.py
@@ -4,24 +4,38 @@ from .models import Museo, Usuario, Comentario
 
 # Create your views here.
 
-def home(request):
+def home(request): #Para el /
 #    return render(request, 'museosMadrid/home.html', {})
     respuesta = '<h2>"Esto es la pagina de inicio (home)"</h2>'
+    return HttpResponse(respuesta)
 
+
+
+def todosLosMuseos(request): #Para el /museos
     Museos = Museo.objects.all()
+    respuesta = '<h2>' + "LISTADO DE TODOS LOS MUSEOS:" + '</h2>'
     for elem in Museos:
         respuesta += '<li>' + '##Datos Museo##' + '</li>'
         respuesta += 'Nombre: ' + elem.nombre + '</br>'
-        respuesta += 'Descripcion: ' + elem.descripcion + '</br>'
+        respuesta += 'Enlace a la web: ' + '<a href="' + '/museos/' + elem.nombre + '">' + elem.nombre + '</a></br>'
         respuesta += '</br>'
 
     return HttpResponse(respuesta)
 
 
-def about(request):
+
+def infoMuseo(request): #Para el /museos/id
+    respuesta = "Datos del museo"
+
+    return HttpResponse(respuesta)
+
+
+
+def about(request): #Para el /about
     respuesta = "Esta es la pagina de about"
     return HttpResponse(respuesta)
 
 
-def error(request):
+
+def error(request): #Para el resto
     return HttpResponse('<h3>Page not found</h3>')
