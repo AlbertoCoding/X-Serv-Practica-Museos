84ca0ed526b21f80daddf799653016bb57cdf4e1
AlbertoCoding <albertor.dev@gmail.com>
2018-04-28 13:35:21 +0200

    Adding some user features

--- museosMadrid/urls.py
+++ museosMadrid/urls.py
@@ -4,9 +4,11 @@ from . import views
 urlpatterns = [
  
     url(r'^about$', views.about),  
+    url(r'^register$', views.register),
   #  url(r'^usuario$', views. ),
   #  url(r'^usuario/', views.) #Para el /usuario/xml
     url(r'^museos$', views.todosLosMuseos),
+    url(r'^museos2$', views.museos_list),
     url(r'^museos/', views.infoMuseo),
     url(r'^$', views.home),
     url(r'^', views.error)
--- museosMadrid/views.py
+++ museosMadrid/views.py
@@ -1,20 +1,25 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Museo, Usuario, Comentario
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def home(request): #Para el /
 #    return render(request, 'museosMadrid/home.html', {})
     respuesta = '<h2>"Esto es la pagina de inicio (home)"</h2>'
-    respuesta += 'Si quieres ver todos los museos: ' + '<a href="' + '/museos">' + 'Listado de museos' + '</a></br>'
+    respuesta += 'Si quieres ver todos los museos: ' + '<a href="' + '/museos">' + 'Todos' + '</a>'
+    respuesta += '</br></br>' + 'About de la web: ' + '<a href="' + '/about">' + 'About' + '</a></br>'
     return HttpResponse(respuesta)
 
 
 
 def todosLosMuseos(request): #Para el /museos
     Museos = Museo.objects.all()
-    respuesta = '<h2>' + "LISTADO DE TODOS LOS MUSEOS:" + '</h2>'
+    respuesta = 'Volver al home: ' + '<a href="' + '/">' + 'Inicio' + '</a>'
+    respuesta += '<h2>' + "LISTADO DE TODOS LOS MUSEOS:" + '</h2>'
     for elem in Museos:
         respuesta += '<li>' + '##Datos Museo##' + '</li>'
         respuesta += 'Id: ' + str(elem.museo_id) + '</br>'
@@ -24,18 +29,50 @@ def todosLosMuseos(request): #Para el /museos
 
     return HttpResponse(respuesta)
 
+def museos_list(request): #Para una alternativa /museos2
+    return render(request, 'museosMadrid/museos_list.html', {})
+
 
 
 def infoMuseo(request): #Para el /museos/id
-    respuesta = "Datos del museo"
+    respuesta = 'Volver a la página de inicio: ' + '<a href="' + '/">' + 'Inicio' + '</a></br>'
+    respuesta += "Datos del museo"
 
     return HttpResponse(respuesta)
 
 
 
 def about(request): #Para el /about
-    respuesta = "Esta es la pagina de about"
+    respuesta = 'Volver a la página de inicio: ' + '<a href="' + '/">' + 'Inicio' + '</a></br>'
+    respuesta += '<h2>Esta es la pagina de about</h1>'
+    respuesta += 'Autor de la página: ' + '<h4>Alberto Rafael Rodríguez Iglesias</h4></br>'
+    respuesta += 'Titulación: ' + '<h4>Grado de Ingeniería en Tecnologías de la Telecomunicación en la Universidad Rey Juan Carlos (URJC)</h4></br>'
+    respuesta += 'Madrid, España</br>'
     return HttpResponse(respuesta)
+
+
+def register(request): #Para el /register
+
+    if request.method == 'GET':
+        respuesta = '<h2>Cree un nuevo usuario</h2></br>'
+        respuesta += '<form action="http://127.0.0.1:8000/register" method"POST">'
+        respuesta += '<label for="id_username">Nombre de usuario: </label> <input id="id_username" name="username" type="text" />'
+        respuesta += '<label for="id_password">Contraseña: </label> <input id="id_password" name="password" type="password" />'
+        respuesta += '<label for="id_email">Correo electronico: </label> <input id="id_email" name="email" type="text" />'
+        respuesta += '<button type="submit">Registrar</button>'
+        respuesta += '</form>'
+        return HttpResponse(respuesta)
+
+    elif request.method == 'POST':
+        username = request.POST.get('username', None) #Para evitar que haya users iguales
+        password = request.POST.get('password', None)
+        email = request.POST.get('email', None)
+        user = User.objects.create_user(username, email, password)
+        #user.last_name = 'Lennon'
+        user.save()
+        return HttpResponse('Usuario creado')
+
+
 
 
 
